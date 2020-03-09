2953fbd6b122342ec58fb1c1f1c33e657de7472b
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-08 22:26:40 -0600

    [ADD] sign up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def create
+    puts params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'users#index'
+  get 'sessions', to: 'sessions#index'
+  post 'sessions', to: 'sessions#create'
+
+  post 'users', to: 'users#create'
 end
